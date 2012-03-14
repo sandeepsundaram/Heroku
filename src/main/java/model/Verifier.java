01008912b8f648eabc3b89d76b596910c563fbb1
Sandeep <ssundaram@apigee.com>
2012-03-14 15:33:06 +0530

    oauth

---
+++ src/main/java/model/Verifier.java
@@ -0,0 +1,30 @@
+package model;
+
+/**
+ * Represents an OAuth verifier code.
+ * 
+ * @author Pablo Fernandez
+ */
+public class Verifier
+{
+
+  private final String value;
+
+  /**
+   * Default constructor.
+   * 
+   * @param value verifier value
+   */
+  public Verifier(String value)
+  {
+	  if (value == null) {
+		  throw new IllegalArgumentException();
+	  }
+    this.value = value;
+  }
+
+  public String getValue()
+  {
+    return value;
+  }
+}

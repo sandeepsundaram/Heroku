132851820d1cc9753eab906ea10ae403d3fb01c2
Sandeep <ssundaram@apigee.com>
2012-03-14 14:41:34 +0530

    oauth

--- src/main/java/servlet/HelloServlet.java
+++ src/main/java/servlet/HelloServlet.java
@@ -11,7 +11,7 @@ import javax.servlet.http.HttpServletResponse;
 
 @WebServlet(
         name = "MyServlet", 
-        urlPatterns = {"/hello"}
+        urlPatterns = {"/hello"}		
     )
 public class HelloServlet extends HttpServlet {
 
@@ -19,7 +19,8 @@ public class HelloServlet extends HttpServlet {
     protected void doGet(HttpServletRequest req, HttpServletResponse resp)
             throws ServletException, IOException {
         ServletOutputStream out = resp.getOutputStream();
-        out.write("hello heroku".getBytes());
+        String code = req.getParameter("code");
+        out.write(("code= "+code).getBytes());
         out.flush();
         out.close();
     }

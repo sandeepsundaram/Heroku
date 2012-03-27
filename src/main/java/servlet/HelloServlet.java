4ebce2a1049faf4abcde30ed099a316cf90f6415
Sandeep <ssundaram@apigee.com>
2012-03-27 22:49:23 +0530

    error fix

--- src/main/java/servlet/HelloServlet.java
+++ src/main/java/servlet/HelloServlet.java
@@ -19,7 +19,6 @@ import model.Response;
 import model.Verb;
 import model.Zodiac;
 
-import org.apache.catalina.Server;
 import org.json.JSONException;
 import org.json.JSONObject;
 import org.json.JSONTokener;
@@ -39,69 +38,84 @@ public class HelloServlet extends HttpServlet {
 	protected void doGet(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
 
-		String code = request.getParameter("code");
+		try {
+			String code = request.getParameter("code");
 
-		String accesstokenURL = "https://graph.facebook.com/oauth/access_token?" + 
-				"client_id=%s&redirect_uri=%s&client_secret=%s&code=%s";
+			String accesstokenURL = "https://graph.facebook.com/oauth/access_token?" + 
+					"client_id=%s&redirect_uri=%s&client_secret=%s&code=%s";
 
-		accesstokenURL = String.format(accesstokenURL, "284063838333812", 
-				URLUtils.urlEncodeWrapper("http://afternoon-galaxy-4740.herokuapp.com/hello"), 
-				"2f00e440583d9fb8384b2a399e22b69e", code);		
+			accesstokenURL = String.format(accesstokenURL, "284063838333812", 
+					URLUtils.urlEncodeWrapper("http://afternoon-galaxy-4740.herokuapp.com/hello"), 
+					"2f00e440583d9fb8384b2a399e22b69e", code);		
 
-		Request req = new Request(Verb.GET, accesstokenURL);
-		Response resp = req.send();
-		if(resp.getCode() == 200) {
-			String body = resp.getBody();
-			if(body != null) {
-				String[] tokens = body.split("&");
-				tokens = tokens[0].split("=");
-				String accessToken = tokens[1];
+			Request req = new Request(Verb.GET, accesstokenURL);
+			Response resp = req.send();
+			if(resp.getCode() == 200) {
+				String body = resp.getBody();
+				if(body != null) {
+					String[] tokens = body.split("&");
+					tokens = tokens[0].split("=");
+					String accessToken = tokens[1];
 
-				OAuthRequest oRequest = new OAuthRequest(Verb.GET, PROTECTED_RESOURCE_URL);
-				oRequest.addQuerystringParameter("access_token", accessToken);
-				Response oResponse = oRequest.send();
-				String fbBody = oResponse.getBody();
-				try {			
-					request.getSession().setAttribute("fbBody", fbBody);	
-					JSONUtils jUtils = new JSONUtils();
-					jUtils.initialize(fbBody);
-					String name = jUtils.getValue("name").getStringValue();
-					String bio = jUtils.getValue("bio").getStringValue();				
-					String dob = jUtils.getValue("birthday").getStringValue();
-					Date date = null;
-					if(dob != null) {
-						SimpleDateFormat formatter = new SimpleDateFormat("MM/dd/yyyy");
-						date = formatter.parse(dob); 
-						request.getSession().setAttribute("dob", dob);	
-					}
-					Zodiac zod = ZodiacUtil.getZodiac(date);
-					zod.setBio(bio);
-					zod.setName(name);
-					request.getSession().setAttribute("zodiac", zod);
+					OAuthRequest oRequest = new OAuthRequest(Verb.GET, PROTECTED_RESOURCE_URL);
+					oRequest.addQuerystringParameter("access_token", accessToken);
+					Response oResponse = oRequest.send();
+					String fbBody = oResponse.getBody();
+					try {			
+						request.getSession().setAttribute("fbBody", fbBody);	
+						JSONUtils jUtils = new JSONUtils();
+						jUtils.initialize(fbBody);
+						String name = jUtils.getValue("name").getStringValue();
+						String bio = jUtils.getValue("bio").getStringValue();				
+						String dob = jUtils.getValue("birthday").getStringValue();
+						Date date = null;
+						if(dob != null) {
+							SimpleDateFormat formatter = new SimpleDateFormat("MM/dd/yyyy");
+							date = formatter.parse(dob); 
+							request.getSession().setAttribute("dob", dob);	
+						}
+						Zodiac zod = ZodiacUtil.getZodiac(date);
+						zod.setBio(bio);
+						zod.setName(name);
+						request.getSession().setAttribute("zodiac", zod);
 
-				} catch (JSONException e) {
-					e.printStackTrace();
-				} catch (ParseException e) {
-					e.printStackTrace();
-				}				
-			} 
-			RequestDispatcher rd = request.getRequestDispatcher("zodiac.jsp");
-			rd.forward(request, response);
-		} else {
-			throw new ServletException(resp.getCode()+"");
-//			String body = resp.getBody();
-//			JSONObject object;
-//			try {
-//				ServletOutputStream out = response.getOutputStream();
-//				object = (JSONObject) new JSONTokener(body).nextValue();
-//				String error = object.getString("error");
-//				out.write(error.getBytes());
-//				out.flush();
-//				out.close();
-//			} catch (Exception e) {
-//				e.printStackTrace();
-//				
-//			}		
+					} catch (JSONException e) {
+						handleError(e, response);
+					} catch (ParseException e) {
+						handleError(e, response);
+					}				
+				} 
+				RequestDispatcher rd = request.getRequestDispatcher("zodiac.jsp");
+				rd.forward(request, response);
+			} else {			
+				String body = resp.getBody();
+				JSONObject object;
+				try {
+					ServletOutputStream out = response.getOutputStream();
+					object = (JSONObject) new JSONTokener(body).nextValue();
+					String error = object.getString("error");
+					out.write(error.getBytes());
+					out.flush();
+					out.close();
+				} catch (Exception e) {
+					handleError(e, response);	
+				}		
+			}
+		}
+		catch (Exception e) {
+			handleError(e, response);				
+		}
+	}
+	
+	private void handleError(Exception e, HttpServletResponse response) {
+		e.printStackTrace();
+		try {
+			ServletOutputStream out = response.getOutputStream();
+			out.write("Test Page".getBytes());
+			out.flush();
+			out.close();
+		} catch (IOException e1) {
+			e1.printStackTrace();
 		}
 	}
 

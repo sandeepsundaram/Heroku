061622b10c0ff8031993f38eafff1ce789e4357e
sandeepsundaram <ssundaram@apigee.com>
2012-04-08 15:23:08 +0530

    test

--- src/main/java/DBUtils/DBUtil.java
+++ src/main/java/DBUtils/DBUtil.java
@@ -9,16 +9,20 @@ import java.sql.Statement;
 
 public class DBUtil {
 
+	private static String dbName = null;
+	
 	public static String action() throws Exception{
 
 		Connection connection = null;
 		try {
 			connection= getConnection();
-
+			
+			System.out.println("DROP TABLE IF EXISTS "+ dbName + ".USER");
+			
 			Statement stmt = connection.createStatement();
-			stmt.executeUpdate("DROP TABLE IF EXISTS USER");
-			stmt.executeUpdate("CREATE TABLE USER (id varchar(10), name varchar(50), dob varchar(50))");
-			stmt.executeUpdate("insert into user (dob, name, id) values ('08-04-2012', 'Sandeep', '2223')");
+			stmt.executeUpdate("DROP TABLE IF EXISTS "+ dbName + ".USER");
+			stmt.executeUpdate("CREATE TABLE "+dbName +".USER (id varchar(10), name varchar(50), dob varchar(50))");
+			stmt.executeUpdate("insert into "+dbName+".user (dob, name, id) values ('08-04-2012', 'Sandeep', '2223')");
 			
 		} catch(Exception e) { 
 			throw e;
@@ -35,7 +39,8 @@ public class DBUtil {
 
 		String username = dbUri.getUserInfo().split(":")[0];
 		String password = dbUri.getUserInfo().split(":")[1];
-		String dbUrl = "jdbc:postgresql://" + dbUri.getHost() + dbUri.getPath();
+		dbName = dbUri.getPath();
+		String dbUrl = "jdbc:postgresql://" + dbUri.getHost() + dbName;
 
 		return DriverManager.getConnection(dbUrl, username, password);
 	}
